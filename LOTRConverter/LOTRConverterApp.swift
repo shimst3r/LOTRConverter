9fe93a978125ded9dfe95dce436f2e6a18a58268
Nils Müller <shimst3r@gmail.com>
2024-11-16 14:44:08 +0100

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Nils Müller on 16.11.24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
